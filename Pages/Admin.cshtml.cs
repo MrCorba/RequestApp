66cd7d71ee5321ad14bda5c9531157fbb0df86b0
Jesper Corba <37336169+MrCorba@users.noreply.github.com>
2025-08-24 13:10:12 +0200

    Add admin page

---
+++ Pages/Admin.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RequestApp.Code.Models;
+using RequestApp.Code.Repository;
+
+namespace RequestApp.Pages;
+
+[Authorize]
+public class AdminModel : PageModel
+{
+    private readonly ISongRepository _repo;
+    public IList<Song> Songs { get;set; } = default!;
+
+    public AdminModel(ISongRepository repo)
+    {
+        this._repo = repo;
+    }
+
+     public async Task OnGetAsync()
+        {
+            await this._repo.AddSongAsync("Title", "Artist");
+
+            Songs = await this._repo.GetAllSongsAsync();
+        }
+}
